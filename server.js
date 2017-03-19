86439b4e87119508f6ed73e5b844a46af4bb647e
Hou Tianze <houtianze@users.noreply.github.com>
2017-03-20 01:39:57 +0800

    Fix typoe

--- server.js
+++ server.js
@@ -185,8 +185,9 @@ app.get("/api/redirect/:encid", function (req, res) {
     var html = `
     <html>
     <head>
-        <meta http-equiv="refresh" content="3;url=http://www.somewhere.com/" />
-    <script>
+        <meta http-equiv="refresh" content="3;url='`
+    html += loc + '" />';
+    html += `<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
