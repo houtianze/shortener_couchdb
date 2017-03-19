2ef82e4e046abf76d022e4195e52597741a43541
Hou Tianze <houtianze@users.noreply.github.com>
2017-03-20 02:00:58 +0800

    Typo

--- server.js
+++ server.js
@@ -186,7 +186,7 @@ app.get("/api/redirect/:encid", function (req, res) {
     <html>
     <head>
         <meta http-equiv="refresh" content="3;url='`
-    html += loc + '" />';
+    html += loc + '\'" />';
     html += `<script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
